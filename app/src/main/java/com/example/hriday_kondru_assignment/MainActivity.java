c954dfcfa9997171b09d4e79ed88bd80bedeaf2a
hridaykondru <hridaykondru@outlook.com>
2023-05-24 18:55:41 +0530

    Fixed blank screen on back gesture in the video list

--- app/src/main/java/com/example/hriday_kondru_assignment/MainActivity.java
+++ app/src/main/java/com/example/hriday_kondru_assignment/MainActivity.java
@@ -17,14 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
-//        sharedViewModel.setVideoList(new ArrayList<>());
-//        sharedViewModel.setCurrentPage(0);
-//        sharedViewModel.setIsFetchingData(false);
         videoListFragment = VideoList.newInstance();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.fragment_container, videoListFragment);
-        transaction.addToBackStack(null); // Add to back stack to allow navigating back
         transaction.commit();
     }
 
@@ -37,36 +33,6 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.fragment_container, videoFragment);
         transaction.addToBackStack(null); // Add to back stack to allow navigating back
         transaction.commit();
-//        Video video = videoList.get(position);
-//        String videoUrl = video.getVideoUrl();
-//
-//        // Create a new instance of MediaPlayer
-//        MediaPlayer mediaPlayer = new MediaPlayer();
-//
-//        try {
-//            // Set the data source to the video URL
-//            mediaPlayer.setDataSource(videoUrl);
-//
-//            // Set the audio stream type
-//            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//
-//            // Set the surface holder for the video playback
-//            mediaPlayer.setDisplay(surfaceHolder);
-//
-//            // Set a listener to handle when the media is prepared
-//            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//                @Override
-//                public void onPrepared(MediaPlayer mp) {
-//                    // Start the video playback
-//                    mediaPlayer.start();
-//                }
-//            });
-//
-//            // Prepare the media asynchronously
-//            mediaPlayer.prepareAsync();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
 }
